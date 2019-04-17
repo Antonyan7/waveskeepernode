9a53997cc76b5177f3907f18950128a321392965
Antonyan7 <you@example.com>
2019-04-17 13:11:43 +0400

    feat: add nodemailer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var mail = require('./mail');
 
 // app.get('/', function(req, res){
 //     res.send('hello world');
 // });
 
+
+app.get('/send', function(req, res){
+  mail.main().catch(console.error);
+});
 
 app.use(express.static('public'))
 
---
+++ mail.js
@@ -0,0 +1,30 @@
+"use strict";
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+async function main(){
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: 'antonyansergey2002@gmail.com', // sender address
+    to: "antonyansergey2002@gmail.com", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+module.exports.main = main;
