b19fdc7c284cf2ee590f6fb25f9e8906631ef629
Tsovak Harutyunyan <tsovakh@gmail.com>
2019-04-17 21:34:14 +0400

    fix: Create smart contracts part

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express()
 const path = require('path')
 const usersRouter = require('./src/routes/users')
 var mail = require('./mail')
+const WavesAPI = require('@waves/waves-api');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_PASS +
@@ -18,61 +19,70 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(usersRouter)
-// app.get('/create-smart-contract', async function(req, res){
-//     // console.log(req)
-//     const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
-//
-//     // Waves.API=
-//
-//     const elephantAccount = Waves.Seed.create();
-//
-//     // To check the validity of tx we need public keys of Inal and Lena
-//     const lenaPubKey = '5XpeKMDVe1AMhuxUdB8dAg1Q26A9XFicvVQohxdcohSb';
-//     const inalPubKey = 'EUxurMktqev3KzBPGD5hhMb6GtyGU2u7sTjaz9DoL73Y';
-//     const scriptBody = `
-//     let lenaPubKey     = base58'${lenaPubKey}'
-//     let inalPubKey     = base58'${inalPubKey}'
-//     let elephantPubKey = base58'${elephantAccount.keyPair.publicKey}'
-//
-//
-//     match tx {
-//       case tx:DataTransaction =>
-//         if(sigVerify(tx.bodyBytes, tx.proofs[0], elephantPubKey)) then true else false
-//       case _ =>
-//         let elephantSigned   = if(sigVerify(tx.bodyBytes, tx.proofs[0], elephantPubKey)) then 1 else 0
-//         let inalSigned       = if(sigVerify(tx.bodyBytes, tx.proofs[1], inalPubKey))     then 1 else 0
-//         let lenaSigned       = if(sigVerify(tx.bodyBytes, tx.proofs[1], lenaPubKey))     then 1 else 0
-//         elephantSigned == 1 && ((inalSigned + lenaSigned) > 0)
-//     }`;
-//     const compiledScript = await Waves.API.Node.utils.script.compile(scriptBody);
-//
-//     // create instance of TransactionWrapper and add signature
-//     const setScriptObj = Object.assign(Helpers.TX_EXAMPLES.SET_SCRIPT, {
-//         script: compiledScript,
-//         sender: elephantAccount.address,
-//         senderPublicKey: elephantAccount.keyPair.publicKey
-//     });
-//     const setScriptTx = await Waves.tools.createTransaction(Waves.constants.SET_SCRIPT_TX_NAME, setScriptObj);
-//     setScriptTx.addProof(elephantAccount.keyPair.privateKey);
-//     console.log(setScriptTx.getTxData());
-//     try {
-//         let a = await setScriptTx.getSignData();
-//         console.log(a);
-//     } catch (e) {
-//         console.log("ERROR");
-//         console.log(e);
-//     }
-//
-//
-//     // send SetScriptTransaction to the network
-//     //to send tx we need TESTNET WAVES on Elephants' account
-//     //you can get them in faucet: https://testnet.wavesexplorer.com/faucet
-//     const txJSON = await setScriptTx.getJSON();
-//     const setScriptResult = await Waves.API.Node.transactions.rawBroadcast(txJSON);
-//
-//     res.send(setScriptResult);
-//
-// });
+
+app.get('/create-smart-contract', async function (req, res) {
+// console.log(req)
+  const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG)
+
+  // Waves.API
+
+  const elephantAccount = Waves.Seed.create()
+
+  // To check the validity of tx we need public keys of Inal and Lena
+  const lenaPubKey = 'HY4NeHugjDMv4U7bxMYc6kpamjNWDs7jKc7xwoUhCHhq'
+  const inalPubKey = 'EUxurMktqev3KzBPGD5hhMb6GtyGU2u7sTjaz9DoL73Y'
+  const scriptBody = `
+    let lenaPubKey     = base58'${lenaPubKey}'
+    let inalPubKey     = base58'${inalPubKey}'
+    let elephantPubKey = base58'HY4NeHugjDMv4U7bxMYc6kpamjNWDs7jKc7xwoUhCHhq' 
+
+
+    match tx {
+      case tx:DataTransaction =>
+        if(sigVerify(tx.bodyBytes, tx.proofs[0], elephantPubKey)) then true else false
+      case _ =>
+        let elephantSigned   = if(sigVerify(tx.bodyBytes, tx.proofs[0], elephantPubKey)) then 1 else 0
+        let inalSigned       = if(sigVerify(tx.bodyBytes, tx.proofs[1], inalPubKey))     then 1 else 0
+        let lenaSigned       = if(sigVerify(tx.bodyBytes, tx.proofs[1], lenaPubKey))     then 1 else 0
+        elephantSigned == 1 && ((inalSigned + lenaSigned) > 0)
+    }`
+  const compiledScript = await Waves.API.Node.utils.script.compile(scriptBody)
+
+  // create instance of TransactionWrapper and add signature
+  const setScriptObj = Object.assign({
+    'type': 12,
+    'version': 1,
+    'fee': 1000000
+  }, {
+    script: compiledScript,
+    sender: '3N6Z2frXZH7tqDbF1U6NcP8mTTxiRaRt7nv', // elephantAccount.address,
+    senderPublicKey: 'HY4NeHugjDMv4U7bxMYc6kpamjNWDs7jKc7xwoUhCHhq'
+  })
+  let setScriptTx
+
+  try {
+    setScriptTx = await Waves.tools.createTransaction(Waves.constants.SET_SCRIPT_TX_NAME, setScriptObj)
+  } catch (e) {
+    console.log(e)
+  }
+
+  setScriptTx.addProof('G87AhfgDvF4SBg6AEBmw7teywNxsB3L4p7VnBaChbrR2')
+  // console.log(setScriptTx.getTxData());
+
+  // send SetScriptTransaction to the network
+  // to send tx we need TESTNET WAVES on Elephants' account
+  // you can get them in faucet: https://testnet.wavesexplorer.com/faucet
+
+  try {
+    const txJSON = await setScriptTx.getJSON()
+    const setScriptResult = await Waves.API.Node.transactions.rawBroadcast(txJSON)
+  } catch (e) {
+    console.log('ASDSD')
+    console.log(e)
+  }
+
+  res.send(setScriptResult)
+})
 
 app.get('/send', function (req, res) {
   mail.main().catch(console.error)
